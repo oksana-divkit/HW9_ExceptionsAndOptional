c1f617c726980ab8706dc042cba0f9f1088a6e98
Oksana Miazina <oksana.myazina@gmail.com>
2020-07-17 17:53:07 +0300

    init

---
+++ src/com/miazina/hw9/Main.java
@@ -0,0 +1,9 @@
+package com.miazina.hw9;
+
+import com.miazina.hw9.executor.Executor;
+
+public class Main {
+    public static void main(String[] args) {
+        Executor.start();
+    }
+}
---
+++ src/com/miazina/hw9/executor/Executor.java
@@ -0,0 +1,8 @@
+package com.miazina.hw9.executor;
+
+import com.miazina.hw9.tasks.*;
+
+public class Executor {
+    public static void start() {
+    }
+}
